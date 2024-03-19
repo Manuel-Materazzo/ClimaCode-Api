c3f263a9ca333faaedf59c5ca1afe7a367dc7581
Manuel <manuelmaterazzo@gmail.com>
2024-03-19 16:32:25 +0100

    first push

---
+++ src/test/java/com/manu/forecaster/ForecasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manu.forecaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForecasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
